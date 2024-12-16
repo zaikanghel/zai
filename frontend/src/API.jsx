4aaa413282f70fdcaa3a952d0185510dabc983a7
zaikanghel <105504978+zaikanghel@users.noreply.github.com>
2024-12-16 22:00:20 +0000

    api file update

--- frontend/src/API.jsx
+++ frontend/src/API.jsx
@@ -1,5 +1,5 @@
 //Auth token we will use to generate a meeting and connect to it
-export const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiIyZDRmOWFmZi1jOGY3LTRjM2ItOTI2OC04NGVjMTUyYjIzNmMiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTczNDIxMzUwOSwiZXhwIjoxNzM0ODE4MzA5fQ.r7opFDeLAbmXnD9WoSSNgg4Qe2vqjV1X6UvkewqQui4";
+export const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI0Y2M3NGRlYi02MDVhLTQ0ZTktOGFkYy1kNmMzZDk2NmI2ZDIiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTczNDM4NjI4MywiZXhwIjoxNzM0OTkxMDgzfQ.ZDSMwaIS8d0rs5SAntfO6dRyc5fA2PMfK72-T_zGuVI";
 // API call to create meeting
 export const createMeeting = async ({ token }) => {
   const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
